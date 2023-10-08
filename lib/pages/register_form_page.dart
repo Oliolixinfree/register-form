57be6ddb471e712d8617e494b6768564d13bbe4b
Oliolixinfree <vanish8ii@gmail.com>
2023-10-08 23:56:07 +0300

    styling input fields, restricting input into form fields, getting text from form fields, form field validation

---
+++ lib/pages/register_form_page.dart
@@ -0,0 +1,296 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class RegisterFormPage extends StatefulWidget {
+  const RegisterFormPage({super.key});
+
+  @override
+  State<RegisterFormPage> createState() => _RegisterFormPageState();
+}
+
+class _RegisterFormPageState extends State<RegisterFormPage> {
+  bool _hidePassword = true;
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _aboutYouController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _phoneController.dispose();
+    _emailController.dispose();
+    _aboutYouController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register Form'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Full Name',
+                hintText: 'What do people call you?',
+                prefixIcon: const Icon(Icons.person),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red[400],
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+              ),
+              validator: _validateName,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _phoneController,
+              decoration: InputDecoration(
+                labelText: 'Phone Number',
+                hintText: 'Where can we reach you?',
+                helperText: 'Phone format: (###)###-####',
+                prefixIcon: const Icon(Icons.call),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red[400],
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+              ),
+              keyboardType: TextInputType.phone,
+              inputFormatters: [
+                // FilteringTextInputFormatter.digitsOnly,
+                FilteringTextInputFormatter(RegExp(r'^[()\d -]{1,15}$'),
+                    allow: true),
+              ],
+              validator: (value) => _validatePhoneNumber(value)
+                  ? null
+                  : 'Phone nuber must be entered as (###)###-####',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter your email address',
+                prefixIcon: Icon(Icons.mail),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: _validateEmail,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _aboutYouController,
+              decoration: const InputDecoration(
+                labelText: 'About You',
+                hintText: 'Tell us about yourself',
+                helperText: 'Keep it short, this is just a demo',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+              ),
+              maxLines: 3,
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(100),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: _hidePassword,
+              maxLength: 8,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter the password',
+                prefixIcon: const Icon(Icons.security),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _hidePassword = !_hidePassword;
+                    });
+                  },
+                  icon: Icon(
+                      _hidePassword ? Icons.visibility : Icons.visibility_off),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+              ),
+              validator: _validatePassword,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _confirmPasswordController,
+              obscureText: _hidePassword,
+              maxLength: 8,
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                hintText: 'Confirm the password',
+                prefixIcon: const Icon(Icons.security),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _hidePassword = !_hidePassword;
+                    });
+                  },
+                  icon: Icon(
+                      _hidePassword ? Icons.visibility : Icons.visibility_off),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+              ),
+              validator: _validatePassword,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _submitForm,
+              child: const Text('Submit Form'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState?.save();
+      print('Form is valid');
+      print('Name: ${_nameController.text}');
+      print('Phone: ${_phoneController.text}');
+      print('Email: ${_emailController.text}');
+      print('About You: ${_aboutYouController.text}');
+      print('Password: ${_passwordController.text}');
+      print('Confirmed Password: ${_confirmPasswordController.text}');
+    } else {
+      print('Form is not valid! Please review and correct');
+    }
+  }
+
+  String? _validateName(String? value) {
+    final nameExp = RegExp(r'^[A-Za-z]+$');
+    if (value!.isEmpty) {
+      return 'Name is required';
+    } else if (!nameExp.hasMatch(value)) {
+      return 'Please enter alphabetical characters';
+    } else {
+      return null;
+    }
+  }
+
+  bool _validatePhoneNumber(String? input) {
+    final phoneExp = RegExp(r'^\(\d\d\d\)\d\d\d\-\d\d\d\d$');
+
+    return phoneExp.hasMatch(input!);
+  }
+
+  String? _validateEmail(String? value) {
+    if (value!.isEmpty) {
+      return 'Email cannot be empty';
+    } else if (!_emailController.text.contains('@')) {
+      return 'Invalid email address';
+    } else {
+      return null;
+    }
+  }
+
+  String? _validatePassword(String? pass) {
+    if (_passwordController.text.length != 8) {
+      return 'Password must be 8 characters long';
+    } else if (_confirmPasswordController.text != _passwordController.text) {
+      return 'Password does not match';
+    } else {
+      return null;
+    }
+  }
+}
